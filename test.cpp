961cf6de25cf287193cf456b773fb54dab95ca5e
chetan <vaishnavchetan701@gmail.com>
2024-10-22 18:49:16 +0530

    second commit

--- test.cpp
+++ test.cpp
@@ -1,11 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a;
-    int b;
-    cout<<"Enter the a and b for addition!";
-    cin>>a>>b;
-    int sum;
-    sum = a+b;
-    cout<<sum;
+    int n;
+    int arr[n];
+    cout<<"Enter the length of an array: ";
+    cin>>n;
+    cout<<"Enter elements of array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Showing the elements";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
 }
