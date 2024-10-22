00843d82794e19b7f6ceaf9e49146dc6a2b5fc4f
chetan <vaishnavchetan701@gmail.com>
2024-10-22 18:21:05 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"Enter the a and b for addition!";
+    cin>>a>>b;
+    int sum;
+    sum = a+b;
+    cout<<sum;
+}
